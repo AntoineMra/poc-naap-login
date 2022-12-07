8fffc9c699edff0cbb64bca1883e7e2f86abbaf2
okylian <kylian.oger@ynov.com>
2022-12-07 16:03:25 +0100

    form components logo

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,16 +1,17 @@
 import '../assets/style/Forms.css';
+import logo from '../assets/img/logo192.png'
 
 import React from 'react'
 
 export const Forms = () => {
     return (
         <div className="test">
-            <p>Se connecter</p>
-        <div className='logo'>
-            <span className='span'>t</span>
-            <span>te</span>
-            <span>tes</span>
-        </div>
+            <p className='title'>Se connecter</p>
+            <div className='icon'>
+                <img className='image' src={logo}></img>
+                <img className='image' src={logo}></img>
+                <img className='image' src={logo}></img>
+            </div>
 
 
         </div>
