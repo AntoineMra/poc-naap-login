7c1b4bfff6d1d529aa8177881ed7d11baabe6660
okylian <kylian.oger@ynov.com>
2022-12-07 14:51:10 +0100

    WIP

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -4,9 +4,13 @@ import React from 'react'
 
 export const Forms = () => {
     return (
-        <div>
+        <div className="test">
             <p>Se connecter</p>
-
+        <div className='logo'>
+            <span className='span'>t</span>
+            <span>te</span>
+            <span>tes</span>
+        </div>
 
 
         </div>
