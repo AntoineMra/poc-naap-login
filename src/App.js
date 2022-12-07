b9d1ebff12dfb6200eca8eaa5bf52e17649076e5
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2022-12-07 14:32:55 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './assets/style/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from './views/Login';
+import { Redirected } from './views/Redirected';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' index={true} element={<Login />} />
+          <Route path='/redirected' element={<Redirected />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
