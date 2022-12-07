13b70a09dba3ea3dfb661241e98a984b6278e995
GurvanN22 <93199510+GurvanN22@users.noreply.github.com>
2022-12-07 13:35:07 +0100

    push the sidebar component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import SideBar from './components/layout/SideBar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
