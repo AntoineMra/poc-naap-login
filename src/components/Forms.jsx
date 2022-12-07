94cbb95ccc29a11103256836c87818a1ad574dcd
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2022-12-07 13:47:23 +0100

    move style forms

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,12 +1,14 @@
+import '../assets/style/Forms.css';
+
 import React from 'react'
 
 export const Forms = () => {
     return (
         <div>
             <p>Se connecter</p>
-            
-            
-            
-            </div>
+
+
+
+        </div>
     )
 }
