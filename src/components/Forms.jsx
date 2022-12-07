ad9d1d0071ed18f02d991cb2131d39c278d14932
okylian <kylian.oger@ynov.com>
2022-12-07 13:42:31 +0100

    add files components

---
+++ src/components/Forms.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Forms = () => {
+    return (
+        <div>
+            <p>Se connecter</p>
+            
+            
+            
+            </div>
+    )
+}
