31e4b06d143b72a6cf0d8a2eeadd6e5a7f79ee25
GurvanN22 <93199510+GurvanN22@users.noreply.github.com>
2023-01-04 13:43:12 +0100

    add email and psw , and redirection

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,9 +1,11 @@
 import logo from '../assets/img/logo192.png'
 import React from 'react'
+import { useState } from "react";
 
 export const Forms = (props) => {
     // TODO: Create a var with useState foreach input (Email & Pwd)
-
+    const [passWord, SetPassWord] = useState("");
+    const [Mail, SetMail] = useState("");
     return (
         <div className="forms-page-container">
             <div className='forms-container'>
@@ -17,14 +19,14 @@ export const Forms = (props) => {
                 <form className='login-form-container'>
                     <div className='input-container'>
                         
-                        <input type="email" className='login-input  login-input-email' />
+                        <input type="email" className='login-input  login-input-email' onChange={(e) => { SetMail(e.target.value)}}/>
                     </div>
                     <div className='input-container'>
                         
-                        <input type="password" className='login-input  login-input-password' />
+                        <input type="password" className='login-input  login-input-password' onChange={(e) => { SetPassWord(e.target.value)}}/>
                     </div>
 
-                    <button className='login-form-submit' type="submit" onClick={props.handleSubmit}>Envoyer</button>
+                    <button className='login-form-submit' type="submit" onClick={() => props.handleSubmit(Mail , passWord)}>Envoyer</button>
                 </form>
             </div>
         </div>
