16673e1060812dfc59e20a7fac6e390fe2a2d1a9
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2023-01-24 19:27:33 +0100

    add fonts / logo

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import Image from "../../assets/img/logo512.png";
+import Image from "../../assets/img/main-logo-dark.png";
 
 export const SideBar = () => {
   return (
     <div className='sidebar'>
-      <input type="checkbox" id="show-menu"/>
+      <input type="checkbox" id="show-menu" />
       <label for="show-menu" className="sidebar-menu-icon"><i className="fa fa-bars"></i></label>
-      <div className="content"> 
+      <div className="content">
         <div className='sidebar-container'>
           <img src={Image} alt='logo naapp' className='sidebar-logo' />
         </div>
-        <p className='sidebar-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam possimus harum nulla expedita quae aperiam numquam voluptatibus quisquam cum, perferendis magni magnam optio rem voluptatum incidunt quia deserunt perspiciatis aliquam.</p>
-        <p className='sidebar-footer'> Made by the best devs of the world in 2022</p>
+        <p className='sidebar-content'>Premier réseau social français dédié à l’accès à l’information sur la neuroatypie.
+          Directement concerné, curieux, ou encore professionnel de santé, partagez vos expériences, demandez des avis et conseils, trouvez des ressources et bien plus encore.</p>
         <div className='sidebar-reseaux'>
           <a href="https://fr.linkedin.com/in/lo%C3%A9lia-galliez-6bb7981b4" target="_blank" rel='noreferrer'>
             <i className="sidebar-socials fa fa-brands fa-linkedin"></i>
@@ -23,8 +23,10 @@ export const SideBar = () => {
             <i className="sidebar-socials fa fa-brands fa-facebook"></i>
           </a>
         </div>
+        <nav>
+          <p className='sidebar-footer'> Made by the best devs of the world in 2022</p>
+        </nav>
       </div>
     </div>
-    );
-  }
-  
+  );
+}
