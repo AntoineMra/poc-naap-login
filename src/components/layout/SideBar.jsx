01a8cea6f0b8403fd30a8f5da401eb753f137c5b
okylian <kylian.oger@ynov.com>
2023-01-25 10:19:34 +0100

    refresh sidebard style

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from "../../assets/img/logo512.png";
+import Image from "../../assets/icons/LOGO_PRINCIPAL_BLANC.svg";
 
 export const SideBar = () => {
   return (
@@ -10,19 +10,24 @@ export const SideBar = () => {
         <div className='sidebar-container'>
           <img src={Image} alt='logo naapp' className='sidebar-logo' />
         </div>
-        <p className='sidebar-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam possimus harum nulla expedita quae aperiam numquam voluptatibus quisquam cum, perferendis magni magnam optio rem voluptatum incidunt quia deserunt perspiciatis aliquam.</p>
-        <p className='sidebar-footer'> Made by the best devs of the world in 2022</p>
-        <div className='sidebar-reseaux'>
-          <a href="https://fr.linkedin.com/in/lo%C3%A9lia-galliez-6bb7981b4" target="_blank" rel='noreferrer'>
-            <i className="sidebar-socials fa fa-brands fa-linkedin"></i>
+        <p className='sidebar-content'>Premier réseau social français dédié à l’accès à l’information sur la neuroatypie.</p>
+<p className="sidebar-content">Directement concerné, curieux, ou encore professionnel de santé, partagez vos expériences, demandez des avis et conseils, trouvez des ressources et bien plus encore.
+</p>
+<p className="sidebar-content-next" >Créez des liens !</p>
+<footer>
+        <div>
+          <a href="https://fr-fr.facebook.com/" target="_blank" rel='noreferrer'>
+            <i className="sidebar-socials fa fa-brands fa-facebook"></i>
           </a>
           <a href="https://www.instagram.com/" target="_blank" rel='noreferrer'>
             <i className="sidebar-socials fa fa-brands fa-instagram"></i>
           </a>
-          <a href="https://fr-fr.facebook.com/" target="_blank" rel='noreferrer'>
-            <i className="sidebar-socials fa fa-brands fa-facebook"></i>
+          <a href="https://fr.linkedin.com/in/lo%C3%A9lia-galliez-6bb7981b4" target="_blank" rel='noreferrer'>
+            <i className="sidebar-socials fa fa-brands fa-linkedin"></i>
           </a>
         </div>
+        <p className='sidebar-footer'><a>Mentions légales</a> | <a>Paramètrer les cookies</a> | <a>Politique de confidentialité</a> | <a>Nous contacter</a> | <a>Aide</a></p>
+        </footer>
       </div>
     </div>
     );
