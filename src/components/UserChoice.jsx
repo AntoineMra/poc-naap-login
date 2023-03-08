e298811704c1aa6fb7db9ba10d8037124c697463
Juliette <juliette.guenard@ynov.com>
2023-03-08 13:29:17 +0100

    css/userChoice page

--- src/components/UserChoice.jsx
+++ src/components/UserChoice.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState } from "react";
+import ChevronR from '../assets/icons/chevronR.svg';
+import ReturnButton from '../assets/icons/ReturnButton.svg';
 
 
 
@@ -12,16 +14,22 @@ export const UserChoice = (props) => {
 
     return (
         <div className="forms-page-container">
-            <button className='ButtonButton-To-Previous-Page' type="submit" onClick={() => props.ReturnToPreviousPage()}/>
             <div className='forms-container'>
+            <div className="previousPageSection">
+            <img src={ReturnButton} alt="Bouton pour retourner à la page précédente" onClick={() => props.ReturnToPreviousPage()} className = "ButtonToPreviousPage"/>
+            </div>
                 <h1 className='form-header-title title-header'>Indiquez nous qui vous êtes !</h1>
+                <h2 className='form-header-subtitle'>Afin de vous orienter au mieux sur Naapp, identifiez vous sous un certain profil</h2>
             <div className="radio-container" onChange={(e) => { SetRole(e.target.value)}}>
-                <input type="radio" id="Professionel" name="role" value="Professionel" className="role"/>
-                <label for="Professionel">Professionel</label>
-                <input type="radio" id="NEUROATYPIQUE" name="role" value="NEUROATYPIQUE" className="role" />
-                <label for="NEUROATYPIQUE">NEUROATYPIQUE</label>
+            <input type="radio" id="NEUROATYPIQUE" name="role" value="NEUROATYPIQUE" className="role" />
+                <label for="NEUROATYPIQUE">Neuroatypique / Proche d’un.e neuroatypique <img src={ChevronR} alt="" className='form-input-icon' /></label>
+                <h2 className='form-header-subtitle2'>Je suis concerné.e par la neuroatypie directement et/ou c’est le cas de quelqu’un dans mon entourage</h2>
                 <input type="radio" id="Curieux" name="role" value="Curieux" className="role"/>
-                <label for="Curieux">Curieux</label>
+                <label for="Curieux">Je suis curieux.se / Je m’informe <img src={ChevronR} alt="" className='form-input-icon' /></label>
+                <h2 className='form-header-subtitle2'>Je ne suis pas directement concerné.e par le sujet mais je souhaite en apprendre plus </h2>
+                <input type="radio" id="Professionel" name="role" value="Professionel" className="role"/>
+                <label for="Professionel">Professionel.le de santé <img src={ChevronR} alt="" className='form-input-icon' /></label>
+                <h2 className='form-header-subtitle2'>Je suis familier/familière avec le sujet grâce à mon métier et je suis apte à délivrer des informations sûres</h2>
                 </div>
             <div className="button-container">
                 <button type="submit"className="button" onClick={() =>props.saveRole(role)}>Suivant</button>
