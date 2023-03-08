b5b0c360c252f9f5bd807de37b353fca8d39204c
okylian <kylian.oger@ynov.com>
2023-03-08 11:18:11 +0100

    css for bio page

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { useState } from "react";
+import ReturnButton from '../assets/img/ReturnButton.png'
 
 export const Biography = (props) => {
 
     const [Bio, SetBio] = useState("");
     return (
 
-        <div className="forms-page-container">
-        <button className='ButtonButton-To-Previous-Page' type="submit" onClick={() => props.ReturnToPreviousPage()}/>
-            <div className='forms-container'>
-                <h1 className='form-header-title'>Dites nous en plus sur vous</h1>
-                <form className='login-form-container'>
+        <div className="login-page-container Email-MDP-flex-column">
+            <div className='Email-MDP-flex'>
+                    <img src={ReturnButton} alt="Bouton pour retourner à la page précédente" onClick={() => props.ReturnToPreviousPage()} className = 'Email-MDP-ButtonToPreviousPage'/>
+                    <h1 className='login-header-title'>Racontez nous qui vous êtes...</h1>
+                </div> 
+            <div className='login-container'>
+                <h2 className='login-header-subtitle'>Afin d’interagir avec les autres membres de Naapp et voir leurs publications, veuillez vous connecter à votre compte.</h2>
+                <form className='form-container'>
                     
-                        <textarea  placeholder='écriver votre biographie' className='Bio-Text-box' onChange={(e) => { SetBio(e.target.value)}}/>
+                        <textarea maxlength="744" placeholder='Votre description (0/###)' className='Bio-Text-box' onChange={(e) => { SetBio(e.target.value)}}/>
                     
-                    <button className='login-form-submit LeftMargin-Button35' type="submit" onClick={() => props.BioPage(Bio)}>Suivant</button>
-                    <button className='Bio-ignore-font' type="submit" onClick={() => props.BioPage("")}>Ignorer</button>
+                    <button className='btn btn-plain form-register-btn Speudo-Bnt-Next-Page' type="submit" onClick={() => props.BioPage(Bio)}>Suivant</button>
+                    <button className='Bio-ignore-font  form-register-btn Speudo-Bnt-Next-Page' type="submit" onClick={() => props.BioPage("")}>Passer cette étape</button>
                     
                 </form>
             </div>
